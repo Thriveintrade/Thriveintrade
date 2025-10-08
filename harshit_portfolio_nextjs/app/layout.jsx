4f1060113999ee93a28d4166bc69810dc003775d
Thrive in trade <101567603+Thriveintrade@users.noreply.github.com>
2025-10-09 02:13:53 +0530

    Update layout.jsx

--- harshit_portfolio_nextjs/app/layout.jsx
+++ harshit_portfolio_nextjs/app/layout.jsx
@@ -1,8 +1,9 @@
-import "./globals.css";
+import "../styles/globals.css";
 
 export const metadata = {
   title: "Harshit Senani Portfolio",
-  description: "Portfolio website of Harshit Senani – Web Developer & SEO Specialist",
+  description:
+    "Portfolio website of Harshit Senani – Web Developer, SEO Specialist & Performance Marketer.",
 };
 
 export default function RootLayout({ children }) {
