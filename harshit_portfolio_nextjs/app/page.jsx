f75e9aff761ad44b0d92e517d047eec3a3fce7f5
Thrive in trade <101567603+Thriveintrade@users.noreply.github.com>
2025-10-09 02:05:58 +0530

    Add files via upload

---
+++ harshit_portfolio_nextjs/app/page.jsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+import HarshitSenaniPortfolio from "../components/harshit_senani_portfolio";
+
+export default function Page() {
+  return <HarshitSenaniPortfolio />;
+}
